01e06266040cbc11780d3ff7a7fe98e9862c8484
Olha Svezhnetseva <svezhentseva@uni-potsdam.de>
2022-09-01 13:50:59 +0200

    Rename program.py in main.py as suggested, add files for classes BKSearch and Chat

---
+++ bk_program/bksearch.py
@@ -0,0 +1,35 @@
+# Olha Svezhentseva
+# 01.09.2022
+
+from bk_program.tree import Tree, METRIC_CLASSES
+
+
+class BKSearcher:
+    "A class responsible for creating and querying BK Tree."
+
+    def __init__(self, words_file: str, calculator_type: str):
+        self.words = self._read_words(words_file)
+        self. distance_calc = METRIC_CLASSES[calculator_type]()
+        self. vis_required = self._visualisation_required()
+        self.tree = self.create_tree()
+
+    def execute_command(self, word: str, d: float) -> list:
+        """The method returns all matches found in a tree based on user's query."""
+        result = self.tree.find_matches(word, float(d))
+        return result
+
+    def create_tree(self) -> Tree:
+        """The method creates a tree and returns its reconstructed version."""
+        tree = Tree(distance_calculator=self.distance_calc, words=self.words)
+        tree.serialize("main_root", "distance_calcualtor")
+        reconstructed_tree = Tree.deserialize("main_root", "distance_calcualtor")
+        return reconstructed_tree
+
+    # Spend too much time checking length of words?
+    def _visualisation_required(self):
+        return len(self.words) <= 50
+
+    @staticmethod
+    def _read_words(path):
+        with open(path) as file_in:
+            return file_in.read().split("\n")
---
+++ bk_program/chat.py
@@ -0,0 +1,81 @@
+# Olha Svezhentseva
+# 01.09.2022
+
+from nltk import word_tokenize
+
+
+class Chat:
+    "A class responsible for communication with the user."
+
+    WARNINGS = {"invalid length": "Input must be in the following format: word number. Try again.",
+                "invalid word": "Your input must begin with a string. Try again.",
+                "invalid distance": "After a word there must be a number. Try again."
+                }
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def show_warning_message(self, status):
+        """The method prints a warning to the user."""
+        return self.WARNINGS[status]
+
+    def run_interactive_mode(self) -> None:
+        """
+        The method is responsible for continuous flow of the program,
+        so that user's input is always awaited.
+        """
+        print(f"The program will now switch to interactive mode. "
+              "Empty input will stop the program.")
+        while True:
+            print(f"Type a word and the maximum distance a string can be from your word "
+                  "and will still be returned. "
+                  "There must be a space between the word and the number.")
+            user_input = input().strip().lower()
+            if user_input == "":
+                print("Bye!")
+                break
+            status, *result = self.check_input(user_input)
+            if status:
+                print(f"Results: {self.bot.execute_command(result[0], result[1])}")
+            else:
+                print(self.show_warning_message(result[0]))
+
+    def check_input(self, user_input: str) -> str or tuple:
+        """The method processes user's input. """
+        user_input = word_tokenize(user_input)
+        if not self._valid_length(user_input):
+            return False, "invalid length"
+
+        word = user_input[0]
+        d = user_input[1]
+
+        if not self._not_number(word):
+            return False, "invalid word"
+
+        if not self._valid_distance(d):
+            return False, "invalid distance"
+        return True, word, d
+
+    @staticmethod
+    def _valid_length(query: str) -> bool:
+        """The method checks input's length."""
+        return len(query) == 2
+
+    @staticmethod
+    def _not_number(word: str) -> bool:
+        """The method checks that a word doesn't solely consist of numbers."""
+        try:
+            int(word)
+        except ValueError:
+            return True
+        return False
+
+    @staticmethod
+    def _valid_distance(d) -> bool:
+        """The method checks that distance can be converted to a float."""
+        try:
+            float(d)
+        except ValueError:
+            return False
+        return True
+
